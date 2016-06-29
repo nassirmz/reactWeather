7817218d981d7b2b4341a71bd9266285ddbf724c
nassir <nassirmz@gmail.com>
2016-06-29 12:14:56 -0700

    (feat) Show a message on loading

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -6,28 +6,44 @@ var openWeatherMap = require('openWeatherMap');
 var Weather = React.createClass({
   getInitialState: function () {
     return {
-      city: 'Miami',
-      temp: 88
+      isLoading: false
     };
   },
   handleSearch: function (city) {
     var that = this;
+
+    that.setState({
+      isLoading: true
+    });
+
     openWeatherMap.getTemp(city).then(function (temp) {
       that.setState({
         city: city,
-        temp: temp
-      })
+        temp: temp,
+        isLoading: false
+      });
     }, function (errorMessage) {
       alert(errorMessage);
+      that.setState({
+        isLoading: false
+      });
     });
   },
   render: function () {
-    var {temp, city} = this.state;
+    var {isLoading, temp, city} = this.state;
+
+    function renderMessage () {
+      if(isLoading) {
+        return <h3>Fetching Weather...</h3>
+      } else if (temp && location) {
+        return <WeatherMessage temp={temp} city={city}/>
+      }
+    }
     return (
       <div>
         <h3>Weather Component</h3>
         <WeatherForm  onSearch={this.handleSearch}/>
-        <WeatherMessage temp={temp} city={city}/>
+        {renderMessage()}
       </div>
     )
   }
