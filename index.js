cb3f2399060b732125b07d3093b676bf1d432882
nassir <nassirmz@gmail.com>
2016-06-29 15:14:10 -0700

    (feat) Prep repo for deployment

--- index.js
+++ index.js
@@ -3,8 +3,18 @@ var bodyParser = require('body-parser');
 var app = express();
 
 app.use(bodyParser.json());
+app.use(function (req, res, next) {
+  if (req.headers['x-forwarded-proto'] ==='http') {
+    next();
+  } else {
+    res.redirect('http://' + req.hostname + req.url);
+  }
+});
 app.use(express.static('public'));
 
-app.listen(3000, function () {
-  console.log('Listening on port 3000');
+const PORT = process.env.PORT || 3000;
+
+
+app.listen(PORT, function () {
+  console.log('Listening on port ', PORT);
 });
